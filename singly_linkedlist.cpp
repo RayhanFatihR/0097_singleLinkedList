56b21ff1c05f76fd085e58d25232b420f8c1d666
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-08 19:31:49 +0700

    Tahap 16

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -135,11 +135,62 @@ int main()
 
         case '2':
         {
-            if 
+            if (mhs.listEmpty())
+            {
+                cout << endl
+                     << "List Kosong" << endl;
+                break;
+            }
+            cout << endl
+                 << "\nMasukkan Nomor Mahasiswa yang akan dihapus: ";
+            cin >> nim;
+            if (mhs.delNode(nim) == false)
+                cout << endl
+                     << "Data tidak ditemukan" << endl;
+            else
+                cout << endl
+                     << "Data dengan Nomor Mahasiswa " << nim << " berhasil dihapus" << endl;
         }
-        
+        break;
+        case '3':
+        {
+            mhs.traverse();
+        }
+        break;
 
-        
-    }
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+            Node * previous, * current;
+            cout << endl
+                 << "Masukkan no mahasiswa yang dicari: ";
+            cin >> nim;
+            if (mhs.Search(nim, &previous, &current) == false)
+                cout << endl
+                     << "Data tidak ditemukan" << endl;
+            else
+            {
+                cout << endl
+                     << "Data ditemukan" << endl;
+                cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                cout << "\n";
+            }
+        }
+        break;
+        case '5':
+        {
+        }
+        break;
+        default:
+        {
+            cout << "Pilihan salah!." << endl;
+        }
+        break;
+        }    
+    }while (ch != '5');
 }
 
